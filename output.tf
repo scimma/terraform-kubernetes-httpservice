710cba3c153762f13503f4e964737d854b7155af
C. Weaver <cnweaver@gmail.com>
2025-01-10 12:23:21 -0500

    Provide the load balancer hostname as an output

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "hostname" {
   description = "Hostname of the application."
   value = local.hostname
 }
+
+output "load_balancer_hostname" {
+  description = "The elastic load balancer allocated for this service"
+  value = kubernetes_service.load_balancer.status[0].load_balancer[0].ingress[0].hostname
+}
